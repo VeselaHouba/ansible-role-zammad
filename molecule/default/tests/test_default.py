b768c02a230db75b17aca01216f93c44a0952f2c
Michalek <michalek@m-cloud.cz>
2021-06-29 18:30:05 +0200

    Role init

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,24 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_docker_is_running(host):
+    cmd = host.service('docker')
+    assert cmd.is_running
+    assert cmd.is_enabled
+
+
+def test_docker_daemon_connection(host):
+    c = host.run('docker ps')
+    assert c.rc == 0
+
+
+def test_docker_container_start(host):
+    with host.sudo():
+        c = host.run('docker run --rm alpine:3.12.0 cat /etc/alpine-release')
+        assert c.rc == 0
+        assert '3.12.0' in c.stdout
